f63ef1fdae08cf4604c4f4f40f7b784d2f9ba768
liaoyj <liaoyj@huak.com.cn>
2020-09-08 23:25:30 +0800

    苦笑，居然有配置文件，又要修改回去

--- sw.js
+++ sw.js
@@ -13,18 +13,18 @@ const CACHE_NAMESPACE = 'main-'
 
 const CACHE = CACHE_NAMESPACE + 'precache-then-runtime';
 const PRECACHE_LIST = [
-  "./",
-  "./offline.html",
-  "./js/jquery.min.js",
-  "./js/bootstrap.min.js",
-  "./js/hux-blog.min.js",
-  "./js/snackbar.js",
-  "./img/icon_wechat.png",
-  "./img/avatar-hux.jpg",
-  "./img/home-bg.jpg",
-  "./img/404-bg.jpg",
-  "./css/hux-blog.min.css",
-  "./css/bootstrap.min.css"
+  "/",
+  "/offline.html",
+  "/js/jquery.min.js",
+  "/js/bootstrap.min.js",
+  "/js/hux-blog.min.js",
+  "/js/snackbar.js",
+  "/img/icon_wechat.png",
+  "/img/avatar-hux.jpg",
+  "/img/home-bg.jpg",
+  "/img/404-bg.jpg",
+  "/css/hux-blog.min.css",
+  "/css/bootstrap.min.css"
   // "//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css",
   // "//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/fonts/fontawesome-webfont.woff2?v=4.6.3",
   // "//cdnjs.cloudflare.com/ajax/libs/fastclick/1.0.6/fastclick.min.js"
